472ba1887c5ec8e3b94000dabdf1fb2ca4c88928
parthapratimsarkar1 <150425513+parthapratimsarkar1@users.noreply.github.com>
2024-12-16 23:37:11 +0530

    Update appV4.py

--- appV4.py
+++ appV4.py
@@ -75,7 +75,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 class CustomerSegmentation:
-    def __init__(self, csv_path='Customer-Dataset-With-Clustered.csv'):
+    def __init__(self, csv_path='/Customer-Dataset-With-ClusteredDB.csv'):
         try:
             # Add error handling for file loading
             if not os.path.exists(csv_path):
